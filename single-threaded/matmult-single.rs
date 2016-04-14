722448ac2b735889175c6c173e83aa28fc0fb1da
Santiago Bonada <Santiago.Bonada@uoit.net>
2016-04-14 00:21:12 -0400

    added a new way to create Vec3s using a form similar to OOP constructors

--- single-threaded/matmult-single.rs
+++ single-threaded/matmult-single.rs
@@ -1,13 +1,19 @@
 // a 3 component vector
-#[allow(dead_code)]
 struct Vec3{
     x: f32,
     y: f32,
     z: f32
 }
 
+// associated functions of Vec3
+impl Vec3{
+    // used for constructing Vec3 nicely
+    fn new(x:f32,y:f32,z:f32) -> Vec3 {
+        Vec3{x: x, y: y, z: z}
+    }
+}
+
 // a 3x3 matrix
-#[allow(dead_code)]
 struct Mat3{
     values: [f32;9]
 }
@@ -16,15 +22,17 @@ fn dot(vec1:&Vec3,vec2:&Vec3) -> f32{
     (vec1.x*vec2.x + vec1.y*vec2.y + vec1.z*vec2.z)
 }
 
+// matrix multiplication of a 3x3 matrix with a 3 dimensional vector (1x3 matrix)
+// returns the result as a vec3
 #[allow(non_snake_case)]
 fn MatXVec3(mat: &Mat3,vec: &Vec3) -> Vec3{
-    let mut result = Vec3{x:0.0,y:0.0,z:0.0};
+    let mut result = Vec3::new(0.0,0.0,0.0);
 
     // construct vectors from the matrix
     let mval = mat.values;
-    let mvec1 = Vec3{x:mval[0],y:mval[1],z:mval[2]};
-    let mvec2 = Vec3{x:mval[3],y:mval[4],z:mval[5]};
-    let mvec3 = Vec3{x:mval[6],y:mval[7],z:mval[8]};
+    let mvec1 = Vec3::new(mval[0],mval[1],mval[2]);
+    let mvec2 = Vec3::new(mval[3],mval[4],mval[5]);
+    let mvec3 = Vec3::new(mval[6],mval[7],mval[8]);
 
     // matrix multiplication is just a bunch of dot products
     result.x = dot(&mvec1,vec);
@@ -42,11 +50,14 @@ fn main(){
         0.0,0.0,1.0]
     };
 
-    let vect = Vec3{x:2.0, y:1.3,z:3.2};
-    let vect2 = Vec3{x:1.4,y:1.2,z:4.3};
-    let vect3 = Vec3{x:0.5,y:0.3,z:2.7};
+    // create some test vectors
+    let vect1 = Vec3::new(2.0, 1.3,3.2);
+    let vect2 = Vec3::new(1.4,1.2,4.3);
+    let vect3 = Vec3::new(0.5,0.3,2.7);
 
-    let product = MatXVec3(&mat_test,&vect);
-    println!("the result is {} {} {}",product.x,product.y,product.z);
+    // multiply the matrix with the vector
+    let product = MatXVec3(&mat_test,&vect1);
+
     println!("the dot product is {}",dot(&vect2,&vect3));
+    println!("the matrix-vector product is is {} {} {}",product.x,product.y,product.z);
 }
